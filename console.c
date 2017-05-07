c99e5b81200d3c99e14d58f79d88ff7868c27710
gahag <gahag@hotmail.com.br>
2017-05-07 20:36:38 -0300

    Update of the console module interface, now using varargs. Documentation.

--- console.c
+++ console.c
@@ -4,9 +4,22 @@
 #include <stdio.h>
 
 
-bool prompt(const char* prompt, const char* format, void* ptr) {
-  assert(prompt != NULL && format != NULL && ptr != NULL);
+bool prompt(const char* prompt, const char* format, ...) {
+  assert(prompt != NULL && format != NULL);
+  
+  va_list va;
+  va_start(va, format);
+  
+  bool result = prompt_va(prompt, format, va);
+  
+  va_end(va);
+  
+  return result;
+}
+
+bool prompt_va(const char* prompt, const char* format, va_list va) {
+  assert(prompt != NULL && format != NULL);
   
   return fputs(prompt, stdout) >= 0
-      && scanf(format, ptr) == 1;
+      && vscanf(format, va) == 1;
 }
--- console.h
+++ console.h
@@ -1,9 +1,17 @@
 #ifndef __CONSOLE_H__
 #define __CONSOLE_H__
 
+#include <stdarg.h>
 #include <stdbool.h>
 
-extern bool prompt(const char* prompt, const char* format, void* ptr);
+
+// Prompt input from stdin. First, fputs the prompt parameter to stdout.
+// Then, scanf with the format and the remaining parameters.
+// Returns wether fputs or scanf succeeded.
+extern bool prompt(const char* prompt, const char* format, ...);
+
+// Varargs version of prompt.
+extern bool prompt_va(const char* prompt, const char* format, va_list);
 
 
 #endif /* __CONSOLE_H__ */
