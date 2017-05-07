3f3b7436450663ad27777a70b803250b55e6f528
gahag <gahag@hotmail.com.br>
2017-05-06 21:54:17 -0300

    Added safety assertion.

--- console.c
+++ console.c
@@ -1,9 +1,12 @@
 #include "console.h"
 
+#include <assert.h>
 #include <stdio.h>
 
 
 bool prompt(const char* prompt, const char* format, void* ptr) {
+  assert(prompt != NULL && format != NULL && ptr != NULL);
+  
   return fputs(prompt, stdout) >= 0
       && scanf(format, ptr) == 1;
 }
