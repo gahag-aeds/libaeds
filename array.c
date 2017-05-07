dc86d314f0dacfae5b0f79c687b7a48c9741b847
gahag <gahag@hotmail.com.br>
2017-05-07 20:37:51 -0300

    Addition of static size specifier for array parameters. Documentation.

--- array.c
+++ array.c
@@ -3,7 +3,7 @@
 #include <assert.h>
 
 
-void array_fill(size_t size, void* array[size], void* value) {
+void array_fill(size_t size, void* array[static size], void* value) {
   assert(array != NULL);
   
   for (size_t i = 0; i < size; i++)
--- array.h
+++ array.h
@@ -4,9 +4,16 @@
 #include <stddef.h>
 
 
+// Short macro for writing for loops iterating over an array.
+// `var` must be a valid unused variable name in the context of the calling site.
+// `from` must be an expression of type size_t. It is evaluated only once.
+// `size` must be an expression of type size_t. It is evaluated once for each iterarion.
 #define foreach(var, from, size) for (size_t var = (from); var < (size); var++)
 
-extern void array_fill(size_t size, void* array[size], void* value);
+// Fill an array of pointers with the given value. O(size) complexity.
+// A for loop with a assignment is used. This guarantees the correct pointer
+// representation is assigned, opposed to using memset.
+extern void array_fill(size_t size, void* array[static size], void* value);
 
 
 #endif /* __ARRAY_H__ */
