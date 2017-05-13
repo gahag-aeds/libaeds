28e3e331bc8977fe74f7f92f7f8800e100cbbacf
gahag <gahag@hotmail.com.br>
2017-05-13 18:55:59 -0300

    Fixed nomenclature. Complexity doc.

--- array.c
+++ array.c
@@ -3,9 +3,10 @@
 #include <assert.h>
 
 
+// O(1)
 void array_fill(size_t size, void* array[static size], void* value) {
   assert(array != NULL);
   
-  for (size_t i = 0; i < size; i++)
-    array[i] = value;
+  for (size_t i = 0; i < size; i++) // O(n)
+    array[i] = value; // O(1)
 }
--- array.h
+++ array.h
@@ -4,15 +4,17 @@
 #include <stddef.h>
 
 
-// Short macro for writing for loops iterating over an array.
+// Short macro for writing for loops iterating an index.
 // `var` must be a valid unused variable name in the context of the calling site.
 // `from` must be an expression of type size_t. It is evaluated only once.
 // `size` must be an expression of type size_t. It is evaluated once for each iterarion.
-#define foreach(var, from, size) for (size_t var = (from); var < (size); var++)
+// Complexity: O(n) where n is the size.
+#define foreach_ix(var, from, size) for (size_t var = (from); var < (size); var++)
 
 // Fill an array of pointers with the given value. O(size) complexity.
 // A for loop with a assignment is used. This guarantees the correct pointer
 // representation is assigned, opposed to using memset.
+// Complexity: O(n) where n is the size of the supplied array.
 extern void array_fill(size_t size, void* array[static size], void* value);
 
 
