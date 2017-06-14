d9707535d23247c362e91f24bc96dc6352ef30cc
gahag <gahag@hotmail.com.br>
2017-06-14 18:33:06 -0300

    Added count_occurrences to array module.

--- data/array.c
+++ data/array.c
@@ -19,3 +19,21 @@ void array_fill(void** restrict  array, size_t size, void* value) {
   for (size_t i = 0; i < size; i++) // O(n)
     array[i] = value; // O(1)
 }
+
+
+// O(n)
+void count_occurrences(
+  void* restrict array,
+  size_t size,
+  size_t elem_size,
+  unsigned long (*key)(void*),
+  unsigned long* restrict keys_counting
+) {
+  assert(array != NULL);
+  assert(elem_size != 0);
+  assert(key != NULL);
+  assert(keys_counting != NULL);
+  
+  foreach_ix (i, 0, size) // O(n)
+    keys_counting[key(array_get(array, elem_size, i))]++; // O(1)
+}
--- data/array.h
+++ data/array.h
@@ -11,6 +11,11 @@
 // Complexity: O(n) where n is the size.
 #define foreach_ix(var, from, size) for (size_t var = (from); var < (size); var++)
 
+// Indexing for void* arrays.
+// array_get(a, elem_size, index) is equivalent to `a + (elem_size * index)`.
+void* array_get(void*, size_t elem_size, size_t index);
+
+
 // Fill an array of pointers with the given value. O(size) complexity.
 // A for loop with a assignment is used. This guarantees the correct pointer
 // representation is assigned, opposed to using memset.
@@ -18,9 +23,20 @@
 void array_fill(void** restrict array, size_t size, void* value);
 
 
-// Indexing for void* arrays.
-// array_get(a, elem_size, index) is equivalent to `a + (elem_size * index)`.
-void* array_get(void*, size_t elem_size, size_t index);
+// Count occurrences of an element in an array.
+// The `keys_counting` parameter is an array to hold how many times each key occurred.
+// The `key` parameter is a function that transforms an element in a key.
+// Elements which are considered equal must evaluate to the same key.
+// The keys are used to index the `keys_counting` array.
+// Therefore, every possible key must be in the bounds of `keys_counting`.
+// Complexity: O(n) where n is the size of the array.
+void count_occurrences(
+  void* restrict array,
+  size_t size,
+  size_t elem_size,
+  unsigned long (*key)(void*),
+  unsigned long* restrict keys_counting
+);
 
 
 #endif /* __LIBAEDS_ARRAY_H__ */
