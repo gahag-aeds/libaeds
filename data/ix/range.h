1d3a23560245f96f9d6383b351ce7eadeb3b4ae8
gahag <gabriel.s.b@live.com>
2017-08-21 01:49:20 -0300

    Added Ix module.

---
+++ data/ix/range.h
@@ -0,0 +1,12 @@
+#ifndef __LIBAEDS_DATA_IX_RANGE_H__
+#define __LIBAEDS_DATA_IX_RANGE_H__
+
+#include <stddef.h>
+
+
+typedef struct IxRange {  // Range of indexes.
+  size_t begin, end;
+} IxRange;
+
+
+#endif /* __LIBAEDS_DATA_IX_RANGE_H__ */
