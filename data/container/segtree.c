6350fa6d0819338de84b0c5d2ef7c833774edf4c
gahag <gabriel.s.b@live.com>
2017-08-27 03:48:32 -0300

    Documentation. Minor improvements.

--- data/container/segtree.c
+++ data/container/segtree.c
@@ -4,17 +4,15 @@
 #include <math.h>
 
 
-struct DataInit {
-  size_t bank_ix;
-  void (*data_init)(void*, void*);
-};
+// Internal helper functions -------------------------------------------------------------
 
-
-
+// Index of a given element in the tree array.
 static size_t segtree_ix(SegTree tree, void** element) {
   return element - tree.tree;
 }
 
+// Get the left child of a given node.
+// If the node is at the bottom of the tree, returns NULL.
 static void** segtree_left(SegTree tree, void** parent) {
   size_t ix = 2 * segtree_ix(tree, parent) + 1;
   
@@ -22,15 +20,25 @@ static void** segtree_left(SegTree tree, void** parent) {
                               : &tree.tree[ix];
 }
 
+// Get the right child of a given node.
+// If the node is at the bottom of the tree, returns NULL.
 static void** segtree_right(SegTree tree, void** parent) {
   size_t ix = 2 * segtree_ix(tree, parent) + 2;
   
   return ix >= tree.tree_size ? NULL
                               : &tree.tree[ix];
 }
+// ---------------------------------------------------------------------------------------
 
 
 
+// new_segtree ---------------------------------------------------------------------------
+struct DataInit { // Struct to hold the data necessary to init a whole tree.
+  size_t nodes_ix;
+  void (*data_init)(void*, void*);
+};
+
+// O(n)
 static void segtree_init(
   SegTree tree,
   Array array,
@@ -38,16 +46,18 @@ static void segtree_init(
   void** node,
   IxRange node_range
 ) {
-  *node = array_get(tree.nodes, di->bank_ix);
-  di->bank_ix++;
+  // Set the current node as the next unused node in tree.nodes:
+  *node = array_get(tree.nodes, di->nodes_ix); 
+  di->nodes_ix++; // Advance the unused node index.
   
   void** left_child = segtree_left(tree, node);
   void** right_child = segtree_right(tree, node);
   
-  if (node_range.begin == node_range.end) {
+  if (node_range.begin == node_range.end) { // The node is a leaf.
     di->data_init(array_get(array, node_range.begin), *node);
     
-    if (left_child != NULL) {
+    if (left_child != NULL) { // If the leaf is not at the bottom of the tree:
+      // Set its childs to NULL.
       *segtree_left(tree, node) = NULL;
       *segtree_right(tree, node) = NULL;
     }
@@ -55,14 +65,19 @@ static void segtree_init(
     return;
   }
   
+  // The node is not a leaf:
+  
+  // Split the node's range for its children:
   IxRange left_range = ixrange_split(&node_range); // node_range is now right_range.
   
-  segtree_init(tree, array, di, left_child, left_range);
-  segtree_init(tree, array, di, right_child, node_range);
+  segtree_init(tree, array, di, left_child, left_range);  // Recurse in the left child.
+  segtree_init(tree, array, di, right_child, node_range); // Recurse in the right child.
   
-  tree.monoid.clear(*node);
-  tree.monoid.append(*node, *left_child);
-  tree.monoid.append(*node, *right_child);
+  // Right and left childs are now initiated,
+  // so init this node with the associative operation of the monoid:
+  monoid_clear(tree.monoid, *node);
+  monoid_append(tree.monoid, *node, *left_child);
+  monoid_append(tree.monoid, *node, *right_child);
 }
 
 SegTree new_segtree(
@@ -76,9 +91,13 @@ SegTree new_segtree(
   assert(array.data != NULL);
   assert(elem_size != 0);
   assert(data_init  != NULL);
-  assert(m.append != NULL && m.clear != NULL);
   
-  size_t data_size = array.size * 2 - 1;
+  // To hold n leaves, it is necessary (2n - 1) nodes (considering leaves are nodes).
+  // The data array contains all nodes and leaves, so 2n -1 is its size.
+  size_t nodes_size = array.size * 2 - 1;
+  // The tree is structured in an array. To do so, the tree is considered complete.
+  // The size of the array must then be 2n' - 1,
+  // where n' is the closest power of 2 that is bigger than n.
   size_t tree_size = 2 * exp2(ceil(log2(array.size))) - 1;
   
   SegTree tree = {
@@ -88,8 +107,8 @@ SegTree new_segtree(
     
     .monoid = m,
     .nodes = (Array) {
-      .data = al_alloc(allocator, data_size, elem_size),
-      .size = data_size,
+      .data = al_alloc(allocator, nodes_size, elem_size),
+      .size = nodes_size,
       .elem_size = elem_size
     },
     
@@ -97,14 +116,21 @@ SegTree new_segtree(
     .tree_size = tree_size
   };
   
-  struct DataInit di = {
-    .bank_ix = 0,
-    .data_init = data_init
-  };
-  segtree_init(tree, array, &di, tree.tree, ixrange(0, tree.source_size - 1));
+  segtree_init(
+    tree,
+    array,
+    &(struct DataInit) {
+      .nodes_ix = 0,
+      .data_init = data_init
+    },
+    tree.tree,
+    ixrange(0, tree.source_size - 1)
+  );
   
   return tree;
 }
+// ---------------------------------------------------------------------------------------
+
 
 void delete_segtree(SegTree* tree) {
   assert(tree != NULL);
@@ -133,7 +159,8 @@ void delete_segtree(SegTree* tree) {
 }
 
 
-
+// segtree_update ------------------------------------------------------------------------
+// O(log n)
 static void segtree_update_(
   SegTree tree,
   IxRange range,
@@ -144,71 +171,84 @@ static void segtree_update_(
   IxRange intersection;
   bool intersects = ixrange_intersect(node_range, range, &intersection);
   
-  if (!intersects)
-    return;
+  if (!intersects)  // If there is no intersection,
+    return;         // the node's range is out of the target range.
   
-  if (node_range.begin == node_range.end) {
+  if (node_range.begin == node_range.end) { // If the node is a leaf, just update it.
     update(*node);
     return;
   }
   
+  // The node is not a leaf and it's part of the target range:
+  
   void** left_child = segtree_left(tree, node);
   void** right_child = segtree_right(tree, node);
   
+  // Split the node's range for its children:
   IxRange left_range = ixrange_split(&node_range); // node_range is now right_range.
   
+  // Recurse in both childs:
   segtree_update_(tree, range, update, left_child, left_range);
   segtree_update_(tree, range, update, right_child, node_range);
   
-  tree.monoid.clear(*node);
-  tree.monoid.append(*node, *left_child);
-  tree.monoid.append(*node, *right_child);
+  // Both children are updated now,
+  // so update this node with the associative operation of the monoid:
+  monoid_clear(tree.monoid, *node);
+  monoid_append(tree.monoid, *node, *left_child);
+  monoid_append(tree.monoid, *node, *right_child);
 }
 
+// O(log n)
 void segtree_update(SegTree tree, IxRange range, void (*update)(void*)) {
   IxRange tree_range = ixrange(0, tree.source_size - 1);
   
   assert(tree.tree != NULL);
-  assert(ixrange_intersect(tree_range, range, NULL));
+  assert(ixrange_within(range, tree_range));
   assert(update != NULL);
   
   segtree_update_(tree, range, update, tree.tree, tree_range);
 }
+// ---------------------------------------------------------------------------------------
 
 
-
+// segtree_query -------------------------------------------------------------------------
+// O(log n)
 static void segtree_query_(
   SegTree tree,
   void** node, IxRange node_range,
   IxRange query, void* out
 ) {
-  if (*node == NULL)
-    return;
-  
   IxRange intersection;
   bool intersects = ixrange_intersect(node_range, query, &intersection);
   
-  if (!intersects)
-    return;
+  if (!intersects)  // If there is no intersection,
+    return;         // the node's range is out of the target range.
   
-  if (ixrange_within(node_range, query))
-    tree.monoid.append(out, *node);
-  else {
-    IxRange left_range = ixrange_split(&node_range);
-    
-    segtree_query_(tree, segtree_left(tree, node), left_range, query, out);
-    segtree_query_(tree, segtree_right(tree, node), node_range, query, out);
+  if (ixrange_within(node_range, query)) {  // If the node's range is completely inside
+    monoid_append(tree.monoid, out, *node); // the query range, then just associate it
+    return;                                 // to the output value.
   }
+  
+  // The node's range is partially within the query range:
+  
+  // Split the node's range for its children:
+  IxRange left_range = ixrange_split(&node_range); // node_range is now right_range.
+  
+  // Recurse for both children:
+  segtree_query_(tree, segtree_left(tree, node), left_range, query, out);
+  segtree_query_(tree, segtree_right(tree, node), node_range, query, out);
 }
 
+// O(log n)
 void segtree_query(SegTree tree, IxRange query, void* out) {
   IxRange tree_range = ixrange(0, tree.source_size - 1);
   
   assert(tree.tree != NULL);
-  assert(ixrange_intersect(tree_range, query, NULL));
+  assert(ixrange_within(query, tree_range));
   assert(out != NULL);
   
-  tree.monoid.clear(out);
+  monoid_clear(tree.monoid, out);
   
   segtree_query_(tree, tree.tree, tree_range, query, out);
 }
+// ---------------------------------------------------------------------------------------
--- data/container/segtree.h
+++ data/container/segtree.h
@@ -7,32 +7,63 @@
 #include <libaeds/memory/allocator.h>
 
 
+// An segment tree is a binary tree used to store information about elements of a
+// collection with respect to their ordering. The tree offers the capability of fast
+// update and query information about a given range inside the collection.
 typedef struct SegTree {
   const Allocator* allocator;
   
-  size_t source_size; // Size of the source array.
+  size_t source_size; // Size of the source array, a.k.a. the number of leaves in the tree.
   
   Monoid monoid;  // The monoid for the tree elements.
-  Array nodes;    // Array of data members: all nodes and leaves. Size: source_size * 2 - 1.
+  Array nodes;    // Array of data members: all nodes and leaves.
+                  // Size: source_size * 2 - 1.
   
   // Array of pointers to represent the tree structure.
-  // Size: 2 * 2^[ceil(log_2(source_size))] - 1
+  // Size: 2 * 2^[ceil(log_2(source_size))] - 1.
   void** tree;
   size_t tree_size;
 } SegTree;
 
 
+// Creates a segtree from the given array.
+// 
+// Parameters:
+// 
+// data_size: The size (bytes) of the data type to contain the
+//            information for any given range in the source array.
+// 
+// data_init: A function to instantiate a leaf of the tree,
+//            from the correspondent element in the source array.
+// 
+// monoid: A monoid for the range information data type.
+//         All nodes except the leaves are generated via
+//         the monoid associative operation on its child.
+// 
+// Complexity: O(n) where n is the source array size.
 SegTree new_segtree(
   const Allocator*,
   Array,
   size_t data_size,
   void (*data_init)(void* array_elem, void* data),
-  Monoid
+  Monoid monoid
 );
+
+// Delete a segtree, deallocating the memory used.
+// Complexity: O(1)
 void delete_segtree(SegTree*);
 
-void segtree_update(SegTree, IxRange ix, void (*update)(void*));
-void segtree_query(SegTree, IxRange ix, void* out);
+
+// Update the leaves of a segtree in the given range via the supplyed the function.
+// The tree is updated to incorporate the changes.
+// Complexity: O(log n) where n is the width of the range.
+void segtree_update(SegTree, IxRange, void (*update)(void*));
+
+// Query the information of the given range in the segtree.
+// The result is stored in the out parameter, which must be
+// a pointer to a value of the range information data type.
+// Complexity: O(log n) where n is the width of the query range.
+void segtree_query(SegTree, IxRange, void* out);
 
 
 #endif /* __LIBAEDS_DATA_CONTAINER_SEGTREE_H__ */
