980930af4479a1110dd4d4a02f0eb098b84a2cc8
gahag <gabriel.s.b@live.com>
2017-09-24 20:24:19 -0300

    Implementation of the bits module.

--- data/bits.c
+++ data/bits.c
@@ -1,9 +1,51 @@
 #include "bits.h"
 
 
-uint8_t popcount_32(uint32_t v) {
-  v -= (v >> 1) & 0x55555555;
-  v = (v & 0x33333333) + ((v >> 2) & 0x33333333);
-  v = (v + (v >> 4)) & 0x0f0f0f0f;
-  return (v * 0x01010101) >> 24;
+uint8_t popcount_8(uint8_t v) {
+  return __builtin_popcount(v);
 }
+
+uint8_t popcount_16(uint16_t v) {
+  return __builtin_popcount(v);
+}
+
+uint8_t popcount_32(uint32_t v) {
+  return __builtin_popcountl(v);
+}
+
+uint8_t popcount_64(uint64_t v) {
+  return __builtin_popcountll(v);
+}
+
+
+#define def_testbit(width) bool testbit_##width(uint##width##_t v, uint8_t b) { \
+  const uint##width##_t _1 = 1;                                                 \
+  return v & (_1 << b);                                                         \
+}
+
+def_testbit(8)
+def_testbit(16)
+def_testbit(32)
+def_testbit(64)
+
+
+#define def_setbit(width) uint##width##_t setbit_##width(uint##width##_t v, uint8_t b) {  \
+  const uint##width##_t _1 = 1;                                                           \
+  return v | (_1 << b);                                                                   \
+}
+
+def_setbit(8)
+def_setbit(16)
+def_setbit(32)
+def_setbit(64)
+
+
+#define def_unsetbit(width) uint##width##_t unsetbit_##width(uint##width##_t v, uint8_t b) {  \
+  const uint##width##_t _1 = 1;                                                               \
+  return v & ~(_1 << b);                                                                      \
+}
+
+def_unsetbit(8)
+def_unsetbit(16)
+def_unsetbit(32)
+def_unsetbit(64)
--- data/bits.h
+++ data/bits.h
@@ -1,10 +1,37 @@
 #ifndef __LIBAEDS_DATA_BITS_H__
 #define __LIBAEDS_DATA_BITS_H__
 
+#include <stdbool.h>
 #include <stdint.h>
 
 
+// popcount: counts the number of set bits in the given value.
+// Complexity: O(1).
+uint8_t popcount_8(uint8_t);
+uint8_t popcount_16(uint16_t);
 uint8_t popcount_32(uint32_t);
+uint8_t popcount_64(uint64_t);
+
+// testbit: returns wheter a given bit is set in the value.
+// Complexity: O(1).
+bool testbit_8(uint8_t, uint8_t);
+bool testbit_16(uint16_t, uint8_t);
+bool testbit_32(uint32_t, uint8_t);
+bool testbit_64(uint64_t, uint8_t);
+
+// setbit: sets the given bit in the value.
+// Complexity: O(1).
+uint8_t  setbit_8(uint8_t, uint8_t);
+uint16_t setbit_16(uint16_t, uint8_t);
+uint32_t setbit_32(uint32_t, uint8_t);
+uint64_t setbit_64(uint64_t, uint8_t);
+
+// unsetbit: clears the given bit in the value.
+// Complexity: O(1).
+uint8_t  unsetbit_8(uint8_t, uint8_t);
+uint16_t unsetbit_16(uint16_t, uint8_t);
+uint32_t unsetbit_32(uint32_t, uint8_t);
+uint64_t unsetbit_64(uint64_t, uint8_t);
 
 
 #endif /* __LIBAEDS_DATA_BITS_H__ */
